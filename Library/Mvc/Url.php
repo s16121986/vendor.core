09720a1e470379f9e2cc1b5b350b29935b9bc886
s16121986 <s16121986@yandex.ru>
2020-03-28 19:51:32 +0300

    Update Url.php

--- Library/Mvc/Url.php
+++ Library/Mvc/Url.php
@@ -53,7 +53,7 @@ class Url extends AbstractUrl{
 	}
 	
 	public function isHome() {
-		return !$this->path || $this->path === $this->homePath;
+		return !$this->path || $this->path === '/';
 	}
 
 }
