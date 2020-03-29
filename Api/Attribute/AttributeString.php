679b95d3e2b7965ed5783ad86e8cadd5b51ad1da
s16121986 <s16121986@yandex.ru>
2020-03-29 16:50:40 +0300

    Update AttributeString.php

--- Api/Attribute/AttributeString.php
+++ Api/Attribute/AttributeString.php
@@ -9,9 +9,12 @@ class AttributeString extends AbstractAttribute{
 	];
 	
 	public function checkValue($value) {
-		if (is_scalar($value)) {
+		if (null === $value && false === $this->notnull)
+			return true;
+		
+		if (is_scalar($value))
 			$value = (string)$value;
-		}
+		
 		return (is_string($value) 
 			&& parent::checkValue($value)
 			&& (strlen($value) >= $this->minlength || ('' === $value && false === $this->notnull)) 
@@ -19,11 +22,13 @@ class AttributeString extends AbstractAttribute{
 	}
 
 	public function prepareValue($value) {
+		if (null === $value && false === $this->notnull)
+			return null;
 		//$value = trim(filter_var($value, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES | FILTER_FLAG_STRIP_LOW));
 		$value = (string)$value;
-		if ($this->length) {
+		if ($this->length)
 			$value = mb_substr($value, 0, $this->length, 'utf-8');
-		}
+		
 		return $value;
 	}
 
