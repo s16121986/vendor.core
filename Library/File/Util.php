7d5668bd9d7aaa5629c1f772433ee75df244f7df
s16121986 <s16121986@yandex.ru>
2020-04-29 23:07:35 +0300

    Update Util.php

--- Library/File/Util.php
+++ Library/File/Util.php
@@ -6,14 +6,13 @@ abstract class Util {
 
 	public static function getMimeType(AbstractFile $file) {
 		$finfo = new \finfo(FILEINFO_MIME_TYPE);
-		if ($file->tmp_name)
+		
+		if ($file->content)
+			return $finfo->buffer($file->content);
+		else if ($file->tmp_name)
 			return $finfo->file($file->tmp_name);
-		elseif ($file->fullname)
-			if ($file->exists())
-				return $finfo->file($file->fullname);
-
-			elseif ($file->content)
-				return $finfo->buffer($file->content);
+		else if ($file->fullname && $file->exists())
+			return $finfo->file($file->fullname);
 
 		return null;
 	}
