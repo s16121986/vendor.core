444ee0f218592ca1d91e1863d9a3552e8e38e780
s16121986 <s16121986@yandex.ru>
2020-07-24 20:42:04 +0300

    Update AttributePassword.php

--- Api/Attribute/AttributePassword.php
+++ Api/Attribute/AttributePassword.php
@@ -1,19 +1,17 @@
 <?php
+
 namespace Api\Attribute;
 
 use Auth;
 
-class AttributePassword extends AttributeString{
+class AttributePassword extends AttributeString {
 
 	protected $qualifiers = [
 		'hidden' => true,
 		'regexp' => false,
 		'length' => 32
 	];
-
-	/*public function checkValue($value) {
-		return parent::checkValue($value) && \Auth\Util::checkPassword($value);
-	}*/
+	private $decryptedValue;
 
 	public function checkValue($value) {
 		return parent::checkValue($value) && (!$this->regexp || preg_match($this->regexp, $value));
@@ -22,6 +20,19 @@ class AttributePassword extends AttributeString{
 	public function prepareValue($value) {
 		$value = parent::prepareValue($value);
 		return Auth::getUser()->getPassword()->encrypt($value);
+	}
+
+	public function getDecryptedValue() {
+		return $this->decryptedValue;
+	}
+
+	public function setValue($value, $forced = false) {
+		if (!parent::setValue($value, $forced))
+			return false;
+
+		$this->decryptedValue = $value;
+
+		return true;
 	}
 
 }
