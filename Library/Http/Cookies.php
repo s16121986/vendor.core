bdc67124af484f5f054ff71158eeada84fd669aa
s16121986 <s16121986@yandex.ru>
2020-09-24 17:26:40 +0300

    Update Cookies.php

--- Library/Http/Cookies.php
+++ Library/Http/Cookies.php
@@ -1,56 +1,60 @@
 <?php
+
 namespace Http;
 
-class Cookies{
-	
+class Cookies {
+
 	private static $domain = null;
-	
+	private static $options = [
+		'path' => '/',
+		'domain' => '', // leading dot for compatibility or use subdomain
+		'secure' => false, // or false
+		'httponly' => true, // or false
+		'samesite' => 'None' // None || Lax || Strict
+	];
+
 	public static function setDomain($domain) {
 		self::$domain = $domain;
 	}
-	
+
+	public static function setOptions(array $options) {
+		foreach ($options as $k => $v) {
+			self::$options[$k] = $v;
+		}
+	}
+
 	public static function get($name, $default = null) {
 		return (isset($_COOKIE[$name]) ? $_COOKIE[$name] : $default);
 	}
-	
+
 	public static function set($name, $cookie, $time, $path = '/', $host = null, $secure = null, $httpOnly = true) {
-		if (null === $secure) {
+		if (null === $secure)
 			$secure = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off');
-		}
-		if (null === $host) $host = self::$domain;
-		if (null === $host && isset($_SERVER['HTTP_HOST'])) {
+
+		if (null === $host)
+			$host = self::$domain;
+		if (null === $host && isset($_SERVER['HTTP_HOST']))
 			$host = $_SERVER['HTTP_HOST'];
-		}
+
 		if (is_string($time)) {
 			$now = now();
 			$now->modify($time);
 			$time = $now->getTimestamp();
-		}if (false && defined('isAdmin')) {
-			var_dump('Set-Cookie:'
-				. ' ' . $name . '=' . $cookie . ';'
-				. ' expires=' . $now->format('D, d-M-Y H:i:s') . ' GMT;'
-				. ' path=' . $path . ';'
-				. ' domain=' . $host
-				. ($secure ? ' secure;' : '')
-				. ($httpOnly ? 'HttpOnly ' : ''));
-			exit;
 		}
-		if (false && $host && strpos($host, '.') !== 0) {
-			header('Set-Cookie:'
-				. ' ' . $name . '=' . $cookie . ';'
-				. ' expires=' . $now->format('D, d-M-Y H:i:s') . ' GMT;'
-				. ' path=' . $path . ';'
-				. ' domain=' . $host);
-			return true;
-		} else {
-			$_COOKIE[$name] = $cookie;
-			return setcookie($name, $cookie, $time, $path, $host, $secure, $httpOnly);
-		}
+
+		$_COOKIE[$name] = $cookie;
+		return setcookie($name, $cookie, array_merge(self::$options, [
+			'expires' => $time,
+			'path' => $path,
+			'domain' => $host,
+			'secure' => $secure,
+			'httponly' => $httpOnly
+		]));
 	}
-	
+
 	public static function clear($name, $path = '/') {
 		unset($_COOKIE[$name]);
 		return self::set($name, '', time() - 3600, $path);
-    }
-	
+	}
+
 }
