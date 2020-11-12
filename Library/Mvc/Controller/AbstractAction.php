4f02a74953d8a27cf412ef67061561a288d7843e
s16121986 <s16121986@yandex.ru>
2020-11-12 12:54:39 +0300

    Update AbstractAction.php

--- Library/Mvc/Controller/AbstractAction.php
+++ Library/Mvc/Controller/AbstractAction.php
@@ -10,7 +10,7 @@ abstract class AbstractAction {
 
 	protected function init() { }
 
-	public function __construct(Controller $controller) {
+	public function __construct(AbstractController $controller) {
 		$this->controller = $controller;
 		$this->init();
 		$a = explode('\\', get_class($this));
