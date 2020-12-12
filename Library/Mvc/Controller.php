21428ec52e2c8ab0250cedef2855442ada16ecf0
s16121986 <s16121986@yandex.ru>
2020-12-13 00:30:40 +0300

    Update Controller.php

--- Library/Mvc/Controller.php
+++ Library/Mvc/Controller.php
@@ -1,10 +1,11 @@
 <?php
+
 namespace Mvc;
 
 use Exception;
 
-abstract class Controller{
-	
+abstract class Controller {
+
 	protected $routes = [
 		//['/action/path/:var/', ['action' => ''], ['var' => 'regexp']]
 		//['/view/:id/', [], ['id' => '\d+']]
@@ -13,59 +14,69 @@ abstract class Controller{
 	protected $data = [];
 	protected $url;
 	protected $view;
-	
-	protected function init() {}
-	
+
+	protected function init() {
+		$this->view->setPaths([
+			'@layout' => 'layout'
+		]);
+	}
+
 	public function __construct(Application $application) {
 		$this->application = $application;
 		$this->url = new Url();
 		$this->view = new View($this);
 		$this->init();
 	}
-	
+
 	public function __get($name) {
+		switch ($name) {
+			case 'application':
+			case 'url':
+				return $this->$name;
+		}
+
 		return $this->get($name);
 	}
-	
+
 	public function __set($name, $value) {
 		$this->set($name, $value);
 	}
-	
+
 	public function get($name) {
 		if ($this->has($name))
 			return $this->data[$name];
 		return $this->application->get($name);
 	}
-	
+
 	public function set($name, $value) {
 		$this->data[$name] = $value;
 	}
-	
+
 	public function has($name) {
 		return array_key_exists($name, $this->data);
 	}
-	
+
 	public function getRequest() {
 		return $this->application->get('request');
 	}
-	
+
 	public function getResponse() {
 		return $this->application->get('response');
 	}
-	
+
 	public function getRoutes() {
 		return $this->routes;
 	}
-	
+
 	public function url($uri = null, $params = null) {
 		$url = clone $this->url;
 		$url->query = null;
 		if ($uri)
 			$url->parse($uri);
-		
+
 		return $url->toString();
 	}
-	
+
 	public function redirect($url = null, $code = 301) {
 		if (is_int($url)) {
 			$code = $url;
@@ -80,13 +91,24 @@ abstract class Controller{
 		}
 		$this->response->setRedirect($url, $code);
 	}
-	
+
+	public function layout($path = null, array $attributes = []) {
+		$layout = '@layout/layout';
+		if (null === $path)
+			$path = ['layout' => $layout];
+		else if (is_array($path))
+			$path['layout'] = $layout;
+		else
+			$attributes['layout'] = $layout;
+		return $this->view->render($path, $attributes);
+	}
+
 	public function render($path, array $attributes = []) {
 		return $this->view->render($path, $attributes);
 	}
-	
+
 	public function onDispatch() {
-		
+
 	}
-	
+
 }
