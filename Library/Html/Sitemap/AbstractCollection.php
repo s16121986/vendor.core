4329322b27be60f25970f201beacff17b0a023b4
s16121986 <s16121986@yandex.ru>
2021-04-14 11:32:34 +0300

    Update AbstractCollection.php

--- Library/Html/Sitemap/AbstractCollection.php
+++ Library/Html/Sitemap/AbstractCollection.php
@@ -11,7 +11,7 @@ abstract class AbstractCollection extends AbstractXml {
 		$this->items[] = $item;
 		return $this;
 	}
-	
+
 	public function out() {
 		header('Content-type: text/xml; charset="UTF-8"');
 		echo $this->__toString();
@@ -19,11 +19,21 @@ abstract class AbstractCollection extends AbstractXml {
 
 	public function __toString() {
 		$xml = '<?xml version="1.0" encoding="UTF-8"?>' . self::NL;
-		$xml .= '<' . $this->tag . '>' . self::NL;
-		foreach ($this->items as $item) {
-			$xml .= (string) $item;
+
+		$xml .= '<' . $this->tag;
+		if ($this->tagAttributes) {
+			foreach ($this->tagAttributes as $k => $v) {
+				$xml .= ' ' . $k . '="' . $v . '"';
+			}
 		}
+		$xml .= '>' . self::NL;
+
+		foreach ($this->items as $item) {
+			$xml .= (string)$item;
+		}
+
 		$xml .= '</' . $this->tag . '>';
+
 		return $xml;
 	}
 
