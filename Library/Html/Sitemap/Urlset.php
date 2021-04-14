bee12b0acf3214411da66ea0578c4a69d377d4e1
s16121986 <s16121986@yandex.ru>
2021-04-14 11:30:00 +0300

    Update Urlset.php

--- Library/Html/Sitemap/Urlset.php
+++ Library/Html/Sitemap/Urlset.php
@@ -7,6 +7,12 @@ use Html\Sitemap\Tag\Url;
 class Urlset extends AbstractCollection {
 
 	protected $tag = 'urlset';
+	protected $tagAttributes = [
+		'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+		'xsi:schemaLocation' => 'http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.w3.org/1999/xhtml http://www.w3.org/2002/08/xhtml/xhtml1-strict.xsd',
+		'xmlns' => 'http://www.sitemaps.org/schemas/sitemap/0.9',
+		'xmlns:xhtml' => 'http://www.w3.org/1999/xhtml'
+	];
 
 	public function addUrl($loc, $lastmod = null, $priority = null, $changefreq = null) {
 		if ($loc instanceof Url) {
