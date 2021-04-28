036689615cb7c2e0079e7e293f314107beac4dd0
s16121986 <s16121986@yandex.ru>
2021-04-29 00:48:57 +0300

    Update Util.php

--- Library/Http/Util.php
+++ Library/Http/Util.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Http;
 
 abstract class Util {
@@ -15,7 +16,8 @@ abstract class Util {
 			'curl',
 			'facebook',
 			'fetch',
-			'okhttp'
+			'okhttp',
+			'Chrome-Lighthouse'
 		];
 		foreach ($bots as $bot) {
 			if (stripos($userAgent, $bot) !== false) {
@@ -25,41 +27,43 @@ abstract class Util {
 		}
 		return false;
 	}
-	
+
 	public static function getHeader($name) {
 
-        // Try to get it from the $_SERVER array first
-        $temp = 'HTTP_' . strtoupper(str_replace('-', '_', $name));
-        if (isset($_SERVER[$temp]) && $_SERVER[$temp])
-            return $_SERVER[$temp];
-		elseif (isset($_SERVER['REDIRECT_' . $temp]) && $_SERVER['REDIRECT_' . $temp])
+		// Try to get it from the $_SERVER array first
+		$temp = 'HTTP_' . strtoupper(str_replace('-', '_', $name));
+		if (isset($_SERVER[$temp]) && $_SERVER[$temp])
+			return $_SERVER[$temp];
+		else if (isset($_SERVER['REDIRECT_' . $temp]) && $_SERVER['REDIRECT_' . $temp])
 			return $_SERVER['REDIRECT_' . $temp];
 
-        // This seems to be the only way to get the Authorization header on
-        // Apache
-        if (function_exists('apache_request_headers')) {
-            $headers = apache_request_headers();
+		// This seems to be the only way to get the Authorization header on
+		// Apache
+		if (function_exists('apache_request_headers')) {
+			$headers = apache_request_headers();
 			$name = strtolower($name);
 			foreach ($headers as $k => $v) {
 				if (strtolower($k) == $name) {
 					return $v;
 				}
 			}
-        } elseif (function_exists('getallheaders')) return getallheaders();
+		} else if (function_exists('getallheaders'))
+			return getallheaders();
 
-        return false;
-    }
-	
+		return false;
+	}
+
 	public static function getHeaders() {
-		 if (function_exists('apache_request_headers'))
-            return apache_request_headers();
-        elseif (function_exists('getallheaders'))
+		if (function_exists('apache_request_headers'))
+			return apache_request_headers();
+		else if (function_exists('getallheaders'))
 			return getallheaders();
 		return [];
 	}
 
 	public static function getUserAgent() {
-		if (!isset($_SERVER['HTTP_USER_AGENT'])) return '';
+		if (!isset($_SERVER['HTTP_USER_AGENT']))
+			return '';
 		return $_SERVER['HTTP_USER_AGENT'];
 	}
 
@@ -69,7 +73,7 @@ abstract class Util {
 			$ip = $_SERVER['HTTP_CLIENT_IP'];
 		} else if ($checkProxy && isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR'] != null) {
 			$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-		} elseif (isset($_SERVER['REMOTE_ADDR'])) {
+		} else if (isset($_SERVER['REMOTE_ADDR'])) {
 			$ip = $_SERVER['REMOTE_ADDR'];
 		}
 		return $ip;
